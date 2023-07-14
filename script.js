0c4c0f3a195c5c8068eff99607801f41fc173533
Dev. Gene <122999167+devgene15@users.noreply.github.com>
2023-07-14 06:19:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 alert("Welcome to Dev. Gene's Portfolio landing page")
+function footeronmouse(){
+    document.getElementById("footer").innerHTML="Wouldn't you rather CODE with us????????????????"
+}
+function footeronmousetwo(){
+    document.getElementById("footer").innerHTML="Your Sleek Websites are in safe hands with Dev. Gene"
+}
